7189b9bda9cd7a4862dbb0ee4fe0a2a4349b8108
Husnain07-git <161119891+Husnain07-git@users.noreply.github.com>
2025-02-24 11:06:06 +0500

    Delete SpringSecurity directory

--- SpringSecurity/src/main/java/com/example/SS/demo/Enums/Roles.java
+++
@@ -1,7 +0,0 @@
-package com.example.SS.demo.Enums;
-
-public enum Roles {
-    USER,
-    ADMIN,
-    SUPER_ADMIN
-}
--- SpringSecurity/src/main/java/com/example/SS/demo/Repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.SS.demo.Repository;
-
-import com.example.SS.demo.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
